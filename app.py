42e4fc7e8daa63f561b62a69cf872aef516c7d1b
bouchrabenali <bouchra.benaliabdallah@epfedu.fr>
2023-09-22 15:29:57 +0200

    third commit

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=G-QJR28CP9SP"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', 'G-QJR28CP9SP');
+</script>
+ """
+ return prefix_google + "Hello World"
+
+
+
+ 
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+ 
+if __name__ == '__main__':
+    app.run()
+
+
+
